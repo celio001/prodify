57fc9c32a627886d82be3b5a7a1dbb0bd3e831d0
celio001 <pessoalcelio01@gmail.com>
2025-11-01 23:25:52 -0300

    test: add test FindAll

--- product/repository_test.go
+++ product/repository_test.go
@@ -53,3 +53,66 @@ func TestGetProduct_Success(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "product1", product.Name)
 }
+
+func TestFindAll_WithPagination(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	repo_product := product.NewRepository(db)
+	
+	product1_uuid := uuid.New()
+	product2_uuid := uuid.New()
+	userid := uuid.New()
+	now := time.Now()
+
+	rows := sqlmock.NewRows([]string{"id", "name", "description", "price", "stock", "createdAt", "updatedAt", "isActive", "userID"}).
+		AddRow(product1_uuid, "product1", "description 1", 200.00, 5, now, now, true, userid).
+		AddRow(product2_uuid, "product2", "description 2", 300.00, 10, now, now, true, userid)
+
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT id, name, description, price, stock, createdAt, updatedAt, isActive, userID 
+	FROM product 
+	ORDER BY createdAt $1 LIMIT $2 OFFSET $3`)).
+		WithArgs("asc", 2, 0). 
+		WillReturnRows(rows)
+
+	products, err := repo_product.FindAll(context.Background(), 1, 2, "asc")
+
+	assert.NoError(t, err)
+	assert.Len(t, products, 2)
+	assert.Equal(t, "product1", products[0].Name)
+	assert.Equal(t, "product2", products[1].Name)
+}
+
+func TestFindAll_WithoutPagination(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	repo_product := product.NewRepository(db)
+	
+	product1_uuid := uuid.New()
+	product2_uuid := uuid.New()
+	product3_uuid := uuid.New()
+	userid := uuid.New()
+	now := time.Now()
+
+	rows := sqlmock.NewRows([]string{"id", "name", "description", "price", "stock", "createdAt", "updatedAt", "isActive", "userID"}).
+		AddRow(product1_uuid, "product1", "description 1", 200.00, 5, now, now, true, userid).
+		AddRow(product2_uuid, "product2", "description 2", 300.00, 10, now, now, true, userid).
+		AddRow(product3_uuid, "product3", "description 3", 400.00, 15, now, now, true, userid)
+
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT id, name, description, price, stock, createdAt, updatedAt, isActive, userID 
+	FROM product 
+	ORDER BY createdAt $1`)).
+		WithArgs("desc"). 
+		WillReturnRows(rows)
+
+	products, err := repo_product.FindAll(context.Background(), 0, 0, "desc")
+
+	assert.NoError(t, err)
+	assert.Len(t, products, 3)
+	assert.Equal(t, "product1", products[0].Name)
+	assert.Equal(t, "product2", products[1].Name)
+	assert.Equal(t, "product3", products[2].Name)
+}
