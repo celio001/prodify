f2c11704126092c45ed948daba1a5cd428d6a435
celio001 <pessoalcelio01@gmail.com>
2025-11-11 21:34:39 -0300

    feat: config stop and start app

--- internal/fiber/server.go
+++ internal/fiber/server.go
@@ -2,14 +2,22 @@ package fiber
 
 import (
 	"context"
+	"fmt"
 
 	v1 "github.com/celio001/prodify/internal/fiber/v1"
 )
 
-func (h HttpServer) Start(ctx context.Context) {
+func (h HttpServer) Start(ctx context.Context) error{
 
 	router := h.app.Group("/api/")
 
 	v1Router := router.Group(v1.HandlerPath)
 	v1.RegisterRouter(v1Router, h.productRepository)
+
+	addr := fmt.Sprint(":8080")
+	return h.app.Listen(addr)
+}
+
+func (h HttpServer) Stop(ctx context.Context) error{
+	return h.app.Shutdown()
 }
