5d7f574f0b18547202d27b2bf882611efd9c3c07
celio001 <pessoalcelio01@gmail.com>
2025-11-18 23:09:52 -0300

    fix: add missing log statement in Start method

--- internal/fiber/server.go
+++ internal/fiber/server.go
@@ -3,21 +3,22 @@ package fiber
 import (
 	"context"
 	"fmt"
+	"log"
 
 	v1 "github.com/celio001/prodify/internal/fiber/v1"
 )
 
-func (h HttpServer) Start(ctx context.Context) error{
-
+func (h HttpServer) Start(ctx context.Context) error {
 	router := h.app.Group("/api/")
 
 	v1Router := router.Group(v1.HandlerPath)
 	v1.RegisterRouter(v1Router, h.productRepository)
 
 	addr := fmt.Sprint(":8080")
+	log.Printf("Starting server on %v", addr)
 	return h.app.Listen(addr)
 }
 
-func (h HttpServer) Stop(ctx context.Context) error{
+func (h HttpServer) Stop(ctx context.Context) error {
 	return h.app.Shutdown()
 }
