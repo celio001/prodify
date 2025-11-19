cbdbaf9fcf8041a33b94533bed86b0dbf46be82e
celio001 <pessoalcelio01@gmail.com>
2025-11-18 23:09:02 -0300

    feat: lifecycle aplication

--- pkg/lifecycle/lifecycle.go
+++ pkg/lifecycle/lifecycle.go
@@ -14,7 +14,6 @@ import (
 func New(ctx context.Context, name string, onStart, onStop func(ctx context.Context) error) {
 	lifeCtx, cancel := context.WithCancel(ctx)
 
-	//Capture system signals
 	go func() {
 		c := make(chan os.Signal, 1)
 		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
@@ -27,20 +26,28 @@ func New(ctx context.Context, name string, onStart, onStop func(ctx context.Cont
 	g, gCtx := errgroup.WithContext(lifeCtx)
 
 	g.Go(func() error {
-		<-gCtx.Done()
+		log.Printf("Starting %s ...", name)
+		if err := onStart(gCtx); err != nil {
+			log.Printf("Error starting %s: %v", name, err)
+			cancel()
+			return err
+		}
+		return nil
+	})
 
+	g.Go(func() error {
+		<-gCtx.Done()
 		log.Printf("Initiating graceful shutdown for %s", name)
 
 		stopCtx, stopCancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer stopCancel()
-
-		err := onStop(stopCtx)
-
-		return err
+		return onStop(stopCtx)
 	})
-	if err := g.Wait(); err != nil{
+
+	// Espera
+	if err := g.Wait(); err != nil {
 		log.Printf("Shutdown completed with error from %s: %s", name, err)
-	}else {
+	} else {
 		log.Printf("Shutdown completed successfully for %s", name)
 	}
 }
