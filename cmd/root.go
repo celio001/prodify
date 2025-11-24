af1ed68ed31520ef1debf05b152680029277fb41
celio001 <pessoalcelio01@gmail.com>
2025-11-23 21:14:34 -0300

    chore: config zap looger

--- cmd/root.go
+++ cmd/root.go
@@ -1,21 +1,21 @@
 package cmd
 
 import (
-	"log"
-
+	"github.com/celio001/prodify/pkg/logger"
 	"github.com/spf13/cobra"
+	"go.uber.org/zap"
 )
 
 var rootCmd = &cobra.Command{
-	Use: "productfy-api",
+	Use:   "productfy-api",
 	Short: "teste teste",
-	Long: "teste teste",
-	Args: cobra.MaximumNArgs(1),
+	Long:  "teste teste",
+	Args:  cobra.MaximumNArgs(1),
 }
 
-func Execute(){
+func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
-		log.Printf("error while executing command")
+		logger.Log.Fatal("error while executing command", zap.String("error", err.Error()))
 	}
 }
