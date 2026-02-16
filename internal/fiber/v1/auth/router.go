5127580999fccaaab5a7e98feded187d6982bb5f
celio001 <pessoalcelio01@gmail.com>
2026-02-16 12:13:33 -0300

    feat(auth): config router auth

--- internal/fiber/v1/auth/router.go
+++ internal/fiber/v1/auth/router.go
@@ -1,1 +1,16 @@
 package auth_handler
+
+import (
+	auth_service "github.com/celio001/prodify/internal/auth/service"
+	"github.com/gofiber/fiber/v2"
+)
+
+const (
+	HandlerPath = "/auth"
+)
+
+func RegisterRouter(router fiber.Router, authService auth_service.AuthService) {
+
+	handler := NewAuthHandler(authService)
+	router.Post("/login", handler.AuthLoginHandler)
+}
