e69061c81ad659f5593f2964ff5434bd9dd2409c
celio001 <pessoalcelio01@gmail.com>
2026-02-16 12:10:35 -0300

    feat(jwt): add token parsing and validation functions

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -1,10 +1,18 @@
 package pkg_jwt
 
 import (
+	"errors"
 	"time"
 
 	"github.com/celio001/prodify/config"
 	"github.com/golang-jwt/jwt/v5"
+)
+
+var (
+	ErrInvalidToken = errors.New("invalid token")
+	ErrInvalidClaims = errors.New("invalid token claims")
+	ErrUserNotFound = errors.New("user not found in token")
+	ErrTokenTypeNotFound = errors.New("token type not found")
 )
 
 // token 15min
@@ -18,4 +26,47 @@ func CreateAccessToken(userID string) (string, error) {
 		})
 
 	return token.SignedString([]byte(config.GetString("JWT_SECRET")))
+}
+
+func ParseToken(tokenString string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(config.GetString("JWT_SECRET")), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, ErrInvalidToken
+	}
+
+	return token, nil
+}
+
+func GetUserIDFromToken(token *jwt.Token) (string, error) {
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return "", ErrInvalidClaims
+	}
+
+	userID, ok := claims["user_id"].(string)
+	if !ok {
+		return "", ErrUserNotFound
+	}
+
+	return userID, nil
+}
+
+func IsAccessToken(token *jwt.Token) (string, error) {
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return "", ErrInvalidClaims
+	}
+
+	tokenType, ok := claims["type"].(string)
+	if !ok {
+		return "", ErrTokenTypeNotFound
+	}
+	return tokenType, nil
 }
